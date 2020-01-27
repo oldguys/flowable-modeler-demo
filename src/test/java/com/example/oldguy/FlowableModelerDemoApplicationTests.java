5f9441aa8f0b9d74a01b48cfa11d069fa15c4201
OldGuys <916812040@qq.com>
2020-01-27 10:37:22 +0800

    flowable设计器抽出

---
+++ src/test/java/com/example/oldguy/FlowableModelerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oldguy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableModelerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
